13a0be9d5f0c080a1b739eaa2b003b6f97203afa
k-kahora <mango22mjk@gmail.com>
2025-10-18 22:20:19 -0400

    I get the token succsfully

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 app_id = os.getenv('APP_ID')
 webhook_secret = os.getenv('WEBHOOK_SECRET')
 app = FastAPI()
-url = "https://api.github.com/user/repos"
+url = "https://api.github.com/app/installations/90642639/access_tokens"
 
 with open(os.environ["PRIVATE_KEY_PATH"], "r") as f:
     private_key_path = f.read()
@@ -26,12 +26,22 @@ def make_jwt():
     }
     return jwt.encode(payload, private_key_path, algorithm="RS256")
 
-async def get_install_token(): 
-    app_jwt = make_jwt()
-    
+def get_install_token(): 
+    token = make_jwt()
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    print("here")
+    res = requests.post(url, headers=headers)
+    res.raise_for_status()
+    data = res.json()
+    print(f"Data received {data}")
+    return data
 
 
 @app.get('/')
 async def root():
     # requests.post(url, data = json.dumps(data),header=headers)
-    return {'message':make_jwt() }
+    return {'message': get_install_token() }
