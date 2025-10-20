543248b9d8446c7160c2b7ce69dab4099225ec27
k-kahora <mango22mjk@gmail.com>
2025-10-20 15:41:01 -0400

    working pull of default branch on install

--- main.py
+++ main.py
@@ -57,11 +57,59 @@ def comment_on_pr(token: str, owner: str, repo: str, pr_number: str):
     res = requests.post(url, headers=headers,json=data)
     print(res.status_code)
     print(res.json())
+    
+def get_default_branch(repo: str, token: str):
+    url = f"https://api.github.com/repos/{repo}"
+    print(url)
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    res = requests.get(url,headers=headers).json()
+    return res["default_branch"]
+     
+    
+@app.get("/test_default_branch")
+def default():
+    token = get_install_token("90854406")
+    url = "https://api.github.com/repos/Yharnix/ci"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    res = requests.get(url,headers=headers).json()
+    return res["default_branch"]
+    
 
 @app.post("/app/webhook")
 async def handle_webhook(request: Request, x_github_event: str | None = Header(None, convert_underscores=False)):
     payload = await request.json()
     event = request.headers.get("X-Github-Event")
+    if event == "installation" and payload.get("action") == "created":
+        print("New app installed can now begin branch creation, pr making and workflow file install")
+        installation_id = payload["installation"]["id"]
+        repositories = payload["repositories"]
+        token = get_install_token(installation_id)
+        for repo in repositories:
+            full_name = repo["full_name"]
+            b_name = get_default_branch(full_name, token)
+            print(b_name)
+
+        # repos = payload["repositories"]
+        # org = payload["organization"]
+        # req = payload["requester"]
+        # sender = payload["sender"]
+        # repo = payload["repository"]
+        # print(f"repo -> {repo}\n org -> {org}\n -> sender -> {sender}\n reqeuster -> {requester}\n repos -> {repos}")
+        # TODO find the default branch 
+        # TODO get the latest sha
+        # TODO make a new branch from the latest sha
+        # TODO make a commit adding the contents to that branch
+        # TODO open a pr adding this workflw file to the repository
+
+
     if event == "pull_request" and payload.get("action") == "opened":
         print("opened pull request")
         installation_id = payload["installation"]["id"]
