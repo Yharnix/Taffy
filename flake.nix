bec8a3b5a0d2454d9b95f7939e23ba8685d5054c
k-kahora <mango22mjk@gmail.com>
2025-10-25 22:34:57 -0400

    back to basics flake

--- flake.nix
+++ flake.nix
@@ -1,113 +1,28 @@
-# SPDX-License-Identifier: Unlicense
-# {
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-#     # Support a particular subset of the Nix systems
-#     # systems.url = "github:nix-systems/default";
-#   };
-
-#   outputs =
-#     { nixpkgs, ... }:
-#     let
-#       eachSystem =
-#         f:
-#         nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
-#     in
-#     {
-#       devShells = eachSystem (pkgs: {
-#         default = pkgs.mkShell {
-#           packages = [
-#             pkgs.nodejs
-#             # pkgs.awscli2
-#             pkgs.python313
-#             pkgs.python313Packages.boto3
-#             pkgs.python313Packages.requests
-
-#             # Alternatively, you can use a specific major version of Node.js
-
-#             # pkgs.nodejs-22_x
-
-#             # Use corepack to install npm/pnpm/yarn as specified in package.json
-#             pkgs.corepack
-#             pkgs.jq
-
-#             # To install a specific alternative package manager directly,
-#             # comment out one of these to use an alternative package manager.
-
-#             # pkgs.yarn
-#             # pkgs.pnpm
-#             # pkgs.bun
-
-#             # Required to enable the language server
-#             # pkgs.nodePackages.typescript
-#             # pkgs.nodePackages.typescript-language-server
-#             # pkgs.nodePackages.aws-cdk
-
-#             # Python is required on NixOS if the dependencies require node-gyp
-
-#             # pkgs.python3
-#           ];
-#           shellHook = ''
-# # have to set npm config path
-
-# npm config set prefix '~/.mutable_npm'
-# mkdir $HOME/.mutable_npm
-# export PATH=$HOME/.mutable_npm/bin:$PATH
-# npm install -g aws-cdk
-# '';
-#         };
-#       });
-#     };
-# }
-
-  
 {
   description = "A simple Python project with NumPy and other packages";
 
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    poetry2nix = {
-        url = "github:nix-community/poetry2nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-utils = {
-        url = "github:numtide/flake-utils";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixgl.url = "github:guibou/nixGL";
+    nixgl.inputs.nixpkgs.follows = "nixpkgs";
   };
-  
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }: 
-        flake-utils.lib.eachDefaultSystem (system: 
-          let
-          pkgs = nixpkgs.legacyPackages.${system};
-          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
-            mkPoetryApplication
-            mkPoetryEnv
-            defaultPoetryOverrides
-          ;
-           poetryDev = mkPoetryEnv {
-             projectDir = self;
-             # overrides = p2n-overrides;
-           };
-          in 
-            {
-
+  outputs = { self, nixpkgs, nixgl }: {
     devShells = {
-      # x86_64-linux = let
-      #   pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      #   pythonEnv = pkgs.python313.withPackages (ps: [
-      #     ps.fastapi
-      #     ps.requests
-      #     ps.pyjwt          # PyJWT
-      #     ps.cryptography   # enables RS256 support in PyJWT
-      #     ps.python-dotenv
-      #     ps.fastapi-cli
-      #     ps.boto3
-      #   ]);
-      # in {
+      x86_64-linux = let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        pythonEnv = pkgs.python313.withPackages (ps: [
+          ps.fastapi
+          ps.requests
+          ps.pyjwt          # PyJWT
+          ps.cryptography   # enables RS256 support in PyJWT
+          ps.python-dotenv
+          ps.fastapi-cli
+          ps.boto3
+        ]);
+      in {
         # Devshell 1 with NumPy and Matplotlib
         default = pkgs.mkShell {
           buildInputs = [
@@ -115,11 +30,9 @@
             pkgs.nodejs
             pkgs.jq
             pkgs.awscli2
-            pkgs.poetry
-            poetryDev
             # pkgs.nodePackages.pyright
             # pkgs.texlive.combined.scheme-full
-            # pythonEnv
+            pythonEnv
             # pkgs.python313Packages.fastapi
             # pkgs.python313Packages.requests
             # pkgs.python313Packages.pyjwt
@@ -139,9 +52,8 @@
     '';
         };
 
-        };
-    
-      });
-
         # Devshell 2 with Pandas and Islp
+      };
+    };
+  };
 }
